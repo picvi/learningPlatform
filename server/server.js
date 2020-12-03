cf01e2a6c604559be5d2a7681b9a66271e52ba70
Sergey <sergey.dyatlov@intexsoft.by>
2020-12-03 13:29:27 +0300

    Refactored server-side

--- server/server.js
+++ server/server.js
@@ -21,31 +21,29 @@ app.post('/add-user', jsonParser, (request, response) => {
   if (!requestedUser) {
     response.status(400).send('Отсутствуют данные о пользователе');
   } else {
-    fs.readFile('./src/assets/db.json', 'utf8', (err, data) => {
+    fs.readFile('./server/db.json', 'utf8', (err, data) => {
       if (err) {
-        response.status(500).send(err);
+        return response.status(500).send(err);
       } else {
         try {
           const db = JSON.parse(data);
           const users = db.users;
-          const userExists = users.some(
-            (user) => user.email === requestedUser.email
-          );
+          const userExists = users.some((user) => user.email === requestedUser.email);
 
           if (userExists) {
-            response.status(400).send('Такой пользователь существует');
+            return response.status(400).send('Такой пользователь существует');
           } else {
             users.push(requestedUser);
-            json = JSON.stringify(db);
-            fs.writeFile('./src/assets/db.json', json, 'utf8', (err) => {
+            json = JSON.stringify(db, null, 2, '\t');
+            fs.writeFile('./server/db.json', json, 'utf8', (err) => {
               if (err) {
-                response.status(500).send(err);
+                return response.status(500).send(err);
               }
-              response.status(200).send('OK');
+              return response.status(200).json('OK');
             });
           }
         } catch (err) {
-          response.status(500).send(err);
+          return response.status(500).send(err);
         }
       }
     });
@@ -53,16 +51,16 @@ app.post('/add-user', jsonParser, (request, response) => {
 });
 
 app.get('/users', (request, response) => {
-  fs.readFile('./src/assets/db.json', 'utf8', (err, data) => {
+  fs.readFile('./server/db.json', 'utf8', (err, data) => {
     if (err) {
-      response.status(500).send(err);
+      return response.status(500).send(err);
     } else {
       try {
         const db = JSON.parse(data);
         const users = db.users;
-        response.send(users);
+        return response.send(users);
       } catch (err) {
-        response.status(500).send(err);
+        return response.status(500).send(err);
       }
     }
   });
@@ -70,34 +68,26 @@ app.get('/users', (request, response) => {
 
 app.post('/login', jsonParser, (request, response) => {
   const requestedUser = request.body;
-  fs.readFile('./src/assets/db.json', 'utf8', (err, data) => {
+  fs.readFile('./server/db.json', 'utf8', (err, data) => {
     if (err) {
-      response.status(500).send(err);
+      return response.status(500).send(err);
     } else {
       try {
         const db = JSON.parse(data);
         const users = db.users;
 
         if (users.length) {
-          const user = users.find(
-            (user) =>
-              user.email === requestedUser.email &&
-              user.pass === requestedUser.pass
-          );
+          const user = users.find((user) => user.email === requestedUser.email && user.pass === requestedUser.pass);
           if (user) {
-            response.status(200).send(JSON.stringify(user));
+            return response.status(200).send(user);
           } else {
-            response
-              .status(400)
-              .send(
-                'Такого пользователя не существует. Пожауйста проверьте еще раз введенные Вами данные!'
-              );
+            return response.status(400).send('Такого пользователя не существует. Пожауйста проверьте еще раз введенные Вами данные!');
           }
         } else {
-          response.status(400).send('Список пользователей пуст');
+          return response.status(400).send('Список пользователей пуст');
         }
       } catch (err) {
-        response.status(500).send(err);
+        return response.status(500).send(err);
       }
     }
   });
@@ -106,7 +96,7 @@ app.post('/login', jsonParser, (request, response) => {
 app.get('/pastSimple', (request, response) => {
   console.log(request);
   let pastSimple = [];
-  fs.readFile('./src/assets/tenses.json', 'utf8', (err, data) => {
+  fs.readFile('./server/tenses.json', 'utf8', (err, data) => {
     if (err) {
       console.log(err);
     } else {
